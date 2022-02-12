fb78846f1efe78e837b889e721b300a0b164ce0b
Jiaming <yejiaming20@126.com>
2022-02-12 16:57:16 +0800

    init

---
+++ output/utg.js
@@ -0,0 +1,137 @@
+var utg = 
+{
+  "nodes": [
+    {
+      "id": "5e81a9b524e53123df2017058fa6b4c8",
+      "shape": "image",
+      "image": "states/screen_2021-12-06_205206.jpg",
+      "label": "QuickstepLauncher\n<FIRST>",
+      "package": "com.android.launcher3",
+      "activity": ".uioverrides.QuickstepLauncher",
+      "state_str": "5e81a9b524e53123df2017058fa6b4c8",
+      "structure_str": "8da684b40757b5046813406328c8a649",
+      "title": "<table class=\"table\">\n<tr><th>package</th><td>com.android.launcher3</td></tr>\n<tr><th>activity</th><td>.uioverrides.QuickstepLauncher</td></tr>\n<tr><th>state_str</th><td>5e81a9b524e53123df2017058fa6b4c8</td></tr>\n<tr><th>structure_str</th><td>8da684b40757b5046813406328c8a649</td></tr>\n</table>",
+      "content": "com.android.launcher3\n.uioverrides.QuickstepLauncher\n5e81a9b524e53123df2017058fa6b4c8\ncom.android.launcher3:id/scrim_view,com.android.quicksearchbox:id/search_plate,com.android.launcher3:id/all_apps_header,com.android.launcher3:id/search_container_workspace,android:id/content,com.android.launcher3:id/page_indicator,com.android.launcher3:id/apps_view,com.android.launcher3:id/launcher,com.android.quicksearchbox:id/search_icon,com.android.launcher3:id/drag_layer,com.android.launcher3:id/qsb_widget,com.android.launcher3:id/hotseat,com.android.quicksearchbox:id/search_widget_text,com.android.launcher3:id/fast_scroller_popup,com.android.launcher3:id/workspace\nCamera,Gallery,Maps,Messaging,Phone,WebView Browser Tester",
+      "font": "14px Arial red"
+    },
+    {
+      "id": "7d3d19f3860b7730d71aff15acabdec1",
+      "shape": "image",
+      "image": "states/screen_2021-12-06_205209.jpg",
+      "label": "WebViewBrowserActivity",
+      "package": "org.chromium.webview_shell",
+      "activity": ".WebViewBrowserActivity",
+      "state_str": "7d3d19f3860b7730d71aff15acabdec1",
+      "structure_str": "e4dea1940ae53c27f779ddd95d4d3144",
+      "title": "<table class=\"table\">\n<tr><th>package</th><td>org.chromium.webview_shell</td></tr>\n<tr><th>activity</th><td>.WebViewBrowserActivity</td></tr>\n<tr><th>state_str</th><td>7d3d19f3860b7730d71aff15acabdec1</td></tr>\n<tr><th>structure_str</th><td>e4dea1940ae53c27f779ddd95d4d3144</td></tr>\n</table>",
+      "content": "org.chromium.webview_shell\n.WebViewBrowserActivity\n7d3d19f3860b7730d71aff15acabdec1\nandroid:id/content,android:id/title,android:id/checkbox\nPrint,Enable tracing,About WebView,Clear cache,Destroy and create new WebView,Animation test"
+    },
+    {
+      "id": "f57f151231b5e5c6876b3a5fd5d0a060",
+      "shape": "image",
+      "image": "states/screen_2021-12-06_205213.jpg",
+      "label": "WebViewBrowserActivity",
+      "package": "org.chromium.webview_shell",
+      "activity": ".WebViewBrowserActivity",
+      "state_str": "f57f151231b5e5c6876b3a5fd5d0a060",
+      "structure_str": "f5185e1105cf7e02a7a628ff6569e6de",
+      "title": "<table class=\"table\">\n<tr><th>package</th><td>org.chromium.webview_shell</td></tr>\n<tr><th>activity</th><td>.WebViewBrowserActivity</td></tr>\n<tr><th>state_str</th><td>f57f151231b5e5c6876b3a5fd5d0a060</td></tr>\n<tr><th>structure_str</th><td>f5185e1105cf7e02a7a628ff6569e6de</td></tr>\n</table>",
+      "content": "org.chromium.webview_shell\n.WebViewBrowserActivity\nf57f151231b5e5c6876b3a5fd5d0a060\norg.chromium.webview_shell:id/url_field,android:id/navigationBarBackground,org.chromium.webview_shell:id/container,android:id/content,android:id/title,android:id/statusBarBackground\nWebView Browser Tester 83.0.4103.106,https://weixin.qq.com/d"
+    },
+    {
+      "id": "88f64d0237deaddfcee2337661b62ea0",
+      "shape": "image",
+      "image": "states/screen_2021-12-06_205225.jpg",
+      "label": "WebViewBrowserActivity\n<LAST>",
+      "package": "org.chromium.webview_shell",
+      "activity": ".WebViewBrowserActivity",
+      "state_str": "88f64d0237deaddfcee2337661b62ea0",
+      "structure_str": "364cbf60134be6c48f009467fb923e9c",
+      "title": "<table class=\"table\">\n<tr><th>package</th><td>org.chromium.webview_shell</td></tr>\n<tr><th>activity</th><td>.WebViewBrowserActivity</td></tr>\n<tr><th>state_str</th><td>88f64d0237deaddfcee2337661b62ea0</td></tr>\n<tr><th>structure_str</th><td>364cbf60134be6c48f009467fb923e9c</td></tr>\n</table>",
+      "content": "org.chromium.webview_shell\n.WebViewBrowserActivity\n88f64d0237deaddfcee2337661b62ea0\nandroid:id/contentPanel,android:id/alertTitle,android:id/parentPanel,android:id/message,android:id/titleDivider,android:id/content,android:id/rightSpacer,android:id/button1,android:id/buttonPanel,android:id/leftSpacer,android:id/scrollView,android:id/title_template,android:id/topPanel\nWebView version : 83.0.4103.106\nenableSmoothTransition : false\ngetAcceptThirdPartyCookies : false\ngetAllowContentAccess : true\ngetAllowFileAccess : true\ngetAllowFileAccessFromFileURLs : false\ngetAllowUniversalAccessFromFileURLs : false\ngetBlockNetworkImage : false\ngetBlockNetworkLoads : false\ngetBuiltInZoomControls : false\ngetCursiveFontFamily : cursive\ngetDatabaseEnabled : true\ngetDatabasePath : \ngetDefaultTextEncodingName : UTF-8\ngetDisplayZoomControls : true\ngetDomStorageEnabled : true\ngetFantasyFontFamily : fantasy\ngetFixedFontFamily : monospace\ngetJavaScriptCanOpenWindowsAutomatically : false\ngetJavaScriptEnabled : true\ngetLightTouchEnabled : false\ngetLoadWithOverviewMode : true\ngetLoadsImagesAutomatically : true\ngetMediaPlaybackRequiresUserGesture : true\ngetNavDump : false\ngetOffscreenPreRaster : false\ngetPluginsEnabled : false\ngetPluginsPath : \ngetSafeBrowsingEnabled : true\ngetSansSerifFontFamily : sans-serif\ngetSaveFormData : false\ngetSavePassword : false\ngetSerifFontFamily : serif\ngetStandardFontFamily : sans-serif\ngetUseDoubleTree : false\ngetUseWebViewBackgroundForOverscrollBackground : false\ngetUseWideViewPort : true\ngetUserAgentString : Mozilla/5.0 (Linux; Android 11; Android SDK built for x86 Build/RSR1.210210.001.A1; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36\ngetVideoOverlayForEmbeddedEncryptedVideoEnabled : false\nsupportMultipleWindows : false\nsupportZoom : true\ntoString : com.android.webview.chromium.ContentSettingsAdapter@d2a998e\n,About WebView,OK",
+      "font": "14px Arial red"
+    }
+  ],
+  "edges": [
+    {
+      "from": "5e81a9b524e53123df2017058fa6b4c8",
+      "to": "7d3d19f3860b7730d71aff15acabdec1",
+      "id": "5e81a9b524e53123df2017058fa6b4c8-->7d3d19f3860b7730d71aff15acabdec1",
+      "title": "<table class=\"table\">\n<tr><th>1</th><td>IntentEvent(intent='am start com.tencent.qqmusic/com.tencent.qqmusic.activity.AppStarterActivity')</td></tr>\n</table>",
+      "label": "1",
+      "events": [
+        {
+          "event_str": "IntentEvent(intent='am start com.tencent.qqmusic/com.tencent.qqmusic.activity.AppStarterActivity')",
+          "event_id": 1,
+          "event_type": "intent",
+          "view_images": []
+        }
+      ]
+    },
+    {
+      "from": "7d3d19f3860b7730d71aff15acabdec1",
+      "to": "f57f151231b5e5c6876b3a5fd5d0a060",
+      "id": "7d3d19f3860b7730d71aff15acabdec1-->f57f151231b5e5c6876b3a5fd5d0a060",
+      "title": "<table class=\"table\">\n<tr><th>2</th><td>TouchEvent(state=7d3d19f3860b7730d71aff15acabdec1, view=b0f1db155a9416e24d528abde17d051d(WebViewBrowserActivity/LinearLayout-))</td></tr>\n</table>",
+      "label": "2",
+      "events": [
+        {
+          "event_str": "TouchEvent(state=7d3d19f3860b7730d71aff15acabdec1, view=b0f1db155a9416e24d528abde17d051d(WebViewBrowserActivity/LinearLayout-))",
+          "event_id": 2,
+          "event_type": "touch",
+          "view_images": [
+            "views/view_b0f1db155a9416e24d528abde17d051d.jpg"
+          ]
+        }
+      ]
+    },
+    {
+      "from": "7d3d19f3860b7730d71aff15acabdec1",
+      "to": "88f64d0237deaddfcee2337661b62ea0",
+      "id": "7d3d19f3860b7730d71aff15acabdec1-->88f64d0237deaddfcee2337661b62ea0",
+      "title": "<table class=\"table\">\n<tr><th>3</th><td>TouchEvent(state=7d3d19f3860b7730d71aff15acabdec1, view=dba62eeb47797e28f87215e5cd54fcfb(WebViewBrowserActivity/TextView-About WebV))</td></tr>\n</table>",
+      "label": "3",
+      "events": [
+        {
+          "event_str": "TouchEvent(state=7d3d19f3860b7730d71aff15acabdec1, view=dba62eeb47797e28f87215e5cd54fcfb(WebViewBrowserActivity/TextView-About WebV))",
+          "event_id": 3,
+          "event_type": "touch",
+          "view_images": [
+            "views/view_dba62eeb47797e28f87215e5cd54fcfb.jpg"
+          ]
+        }
+      ]
+    },
+    {
+      "from": "f57f151231b5e5c6876b3a5fd5d0a060",
+      "to": "7d3d19f3860b7730d71aff15acabdec1",
+      "id": "f57f151231b5e5c6876b3a5fd5d0a060-->7d3d19f3860b7730d71aff15acabdec1",
+      "title": "<table class=\"table\">\n<tr><th>2</th><td>TouchEvent(state=f57f151231b5e5c6876b3a5fd5d0a060, view=6872c84f392fc0f1759a52f265180d81(WebViewBrowserActivity/ImageButton-))</td></tr>\n</table>",
+      "label": "2",
+      "events": [
+        {
+          "event_str": "TouchEvent(state=f57f151231b5e5c6876b3a5fd5d0a060, view=6872c84f392fc0f1759a52f265180d81(WebViewBrowserActivity/ImageButton-))",
+          "event_id": 2,
+          "event_type": "touch",
+          "view_images": [
+            "views/view_6872c84f392fc0f1759a52f265180d81.jpg"
+          ]
+        }
+      ]
+    }
+  ],
+  "num_nodes": 4,
+  "num_edges": 4,
+  "num_effective_events": 3,
+  "num_reached_activities": 0,
+  "test_date": "2021-12-06 20:51:58",
+  "time_spent": 26.98733,
+  "num_transitions": 6,
+  "device_serial": "emulator-5554",
+  "device_model_number": "Android SDK built for x86",
+  "device_sdk_version": 30,
+  "app_sha256": "6e83533fcd621c47299f6fedce394c885a618ba829597ae46e9fa3bf14664489",
+  "app_package": "com.tencent.qqmusic",
+  "app_main_activity": "com.tencent.qqmusic.activity.AppStarterActivity",
+  "app_num_total_activities": 295
+}
